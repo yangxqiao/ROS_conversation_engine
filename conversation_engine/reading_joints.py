5f968d1009158c3d09b62918d40f19ba37659765
Xiaoyang Qiao <xiaoyanq@usc.edu>
2019-06-28 17:10:29 -0700

    Add three test files

---
+++ conversation_engine/reading_joints.py
@@ -0,0 +1,14 @@
+import rospy
+from sensor_msgs.msg import JointState
+
+def joint_states_callback(msg):
+    strmsg = ""
+    for i, joint_name in enumerate(msg.name):
+        strmsg += "%s: %.2f, " % (joint_name, msg.position[i])
+    rospy.loginfo(strmsg)
+
+if __name__ == '__main__':
+    rospy.init_node('joints_example')
+    rospy.Subscriber('/qt_robot/joints/state', JointState, joint_states_callback)
+
+    rospy.spin()
