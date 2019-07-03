50696bcead08aacc92650102bb214b5b2b7f40c5
Xiaoyang <xiaoyanq@usc.edu>
2019-07-02 17:15:01 -0700

    Fix reading_head_position.py

--- conversation_engine/reading_head_position.py
+++ conversation_engine/reading_head_position.py
@@ -3,15 +3,19 @@ import rospy
 from qt_nuitrack_app.msg import Faces
 
 def head_callback(msg):
-    strmsg = "---------------------'\n'"
-    for i in enumerate(msg.left_eye):
-        strmsg += "left_eye: %.2f'\n'" % (msg.left_eye[i])
-    for i in enumerate(msg.right_eye):
-    	strmsg += "right_eye: %.2f'\n'" % (msg.right_eye[i])
-    rospy.loginfo(strmsg)
+#    strmsg = "---------------------'\n'"
+#    strmsg += "left_eye: %.2f'\n'" % (msg.faces.left_eye[0])
+#    strmsg += "left_eye: %.2f'\n'" % (msg.faces.left_eye[1])
+#    strmsg += "right_eye: %.2f'\n'" % (msg.faces.right_eye[0])
+#    strmsg += "right_eye: %.2f'\n'" % (msg.faces.right_eye[1])
+  	
+    # rospy.loginfo(type(msg.faces))
+    rospy.loginfo(msg.faces[0].left_eye)
+    # rospy.loginfo(len(msg.faces))
 
 if __name__ == '__main__':
     rospy.init_node('reading_head_example')
+    print("Initialize the node")
 
     # create subscriber
     rospy.Subscriber('/qt_nuitrack_app/faces', Faces, head_callback)
