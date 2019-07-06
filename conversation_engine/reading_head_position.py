aaa7606a46d4b7f735f56f7dfed07424a12e4fef
Xiaoyang <xiaoyanq@usc.edu>
2019-07-06 08:53:19 -0700

    Add PositionInfo class

--- conversation_engine/reading_head_position.py
+++ conversation_engine/reading_head_position.py
@@ -3,12 +3,36 @@ import rospy
 from qt_nuitrack_app.msg import Faces
 from std_msgs.msg import String
 
+class PositionInfo:
+    def __init__(self):
+        self.left_eye = [0.5, 0.5]
+        self.right_eye = [0.5, 0.5]
+
+    def get_left_eye(self):
+        return self.left_eye
+
+    def get_right_eye(self):
+        return self.right_eye
+
+    def set_left_eye(self, new_position):
+        self.left_eye[0] = new_position[0]
+        self.left_eye[0] = new_position[1]
+
+    def set_right_eye(self, new_position):
+        self.right_eye[0] = new_position[0]
+        self.right_eye[1] = new_position[1]
+
+
 def head_callback(msg):
     global count
     count = count + 1
     if count == 5:
+
         strmsg_left = "Left eye: (%.4f, %.4f)" % (msg.faces[0].left_eye[0], msg.faces[0].left_eye[1])
         strmsg_right = "Right eye: (%.4f, %.4f)" % (msg.faces[0].right_eye[0], msg.faces[0].right_eye[1])
+
+        my_position.set_left_eye(msg.faces[0].left_eye)
+        my_position.set_right_eye(msg.faces[0].right_eye)
 
         show_expression("QT/happy")
         play_gesture("QT/happy")
@@ -20,9 +44,11 @@ def head_callback(msg):
 
         count = 0
 
+
 def show_expression(file_path):
 	emotionShow_pub = rospy.Publisher('/qt_robot/emotion/show', String, queue_size=1)
 	emotionShow_pub.publish(file_path)
+
 
 def play_gesture(file_path):
     gesturePlay_pub = rospy.Publisher('/qt_robot/gesture/play', String, queue_size=1)
@@ -34,6 +60,7 @@ if __name__ == '__main__':
     print("Initialize the node")
 
     count = 0
+    my_position = PositionInfo()
     # create subscriber
     rospy.Subscriber('/qt_nuitrack_app/faces', Faces, head_callback)
 
