bf561f24c0d16a618b557a7c4ebf947bba2a17f3
Xiaoyang <xiaoyanq@usc.edu>
2019-07-08 10:47:19 -0700

    Try run bash command in python script

--- conversation_engine/commanding_head.py
+++ conversation_engine/commanding_head.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import rospy
 from std_msgs.msg import Float64MultiArray
+import subprocess
 
 def move_head(HeadYaw, HeadPitch):
     ref = Float64MultiArray()
@@ -9,7 +10,6 @@ def move_head(HeadYaw, HeadPitch):
     # my_dict = {'label': '', 'size': 0, 'stride': 0}
 
     print(ref.layout.dim[0])
-
     print(type(ref.layout.dim))
     print(ref)
 
@@ -18,11 +18,12 @@ def move_head(HeadYaw, HeadPitch):
 if __name__ == '__main__':
     rospy.init_node('moving_head_example', anonymous=True)
     
-    # create pubisher
+    # create the pubisher
     head_pub = rospy.Publisher('/qt_robot/head_position/command', Float64MultiArray, queue_size=1)
 
     try:
-        move_head(2, 0)
+        # move_head(2, 0)
+        subprocess.check_call(["rostopic", "pub", "/qt_robot/head_position/command std_msgFloat64MultiArray", "'{layout:{dim:[{label: '', size: 0, stride: 0}], data_offset: 0}, data: [-2, 0]}'"])
         rospy.spin()
     except rospy.ROSInterruptException:
         pass
